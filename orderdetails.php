ba1771429a2d3990817279c01edac408511aafec
abhisheknitdgp123 <abhisheknitdgp123@gmail.com>
2015-09-22 11:58:06 +0530

    Create orderdetails.php

---
+++ orderdetails.php
@@ -0,0 +1,6 @@
+<?php
+$a="<table border='2' color='black'><thead><tr><th>order no<th>date of order<th>product details<th>status<th>AWB No.</tr></thead>";
+$b="<tbody><tr><td><td><td><td><td></tr><tr><td><td><td><td><td></tr><tr><td><td><td><td><td></tr></tbody></table>";
+$c=$a.$b;
+echo $c;
+?>
